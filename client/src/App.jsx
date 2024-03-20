bb04ceaaeb8fd1ed8d4b0ce11db05365f4e9e00c
Flavius <flavius.carpean@student.upt.ro>
2024-03-20 16:45:01 +0200

    am creat paginile si routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
 
 export default function App() {
   return (
-    <h1 className = 'text-3xl text-red-500'>app</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/home" element = {<Home/>} />
+      <Route path = "/about" element = {<About/>}/>
+      <Route path = "/sign-up" element = {<SignUp />}/>
+      <Route path = "/projects" element = {<Projects />}/>
+      <Route path = "/sign-in" element = {<SignIn />}/>
+      <Route path = "/dashboard" element = {<Dashboard/>}/>
+      <Route/>
+    </Routes>
+    </BrowserRouter>
   )
 }

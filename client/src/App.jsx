94633fe0ffebf458d70b51198ab6f5037b12a301
Flavius <flavius.carpean@student.upt.ro>
2024-03-24 22:18:10 +0200

    Am facut dashboard-ul sa fie privat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -18,8 +19,11 @@ export default function App() {
       <Route path = "/about" element = {<About/>}/>
       <Route path = "/sign-up" element = {<SignUp />}/>
       <Route path = "/projects" element = {<Projects />}/>
+      <Route element = {<PrivateRoute/>}>
+        <Route path = "/dashboard" element = {<Dashboard/>}/>
+      </Route>
       <Route path = "/sign-in" element = {<SignIn />}/>
-      <Route path = "/dashboard" element = {<Dashboard/>}/>
+      
       
     </Routes>
     <Footer/>

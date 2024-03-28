a3c32f7a0c53d11a4fc507e5bc05c9a2f2a2a526
Flavius <flavius.carpean@student.upt.ro>
2024-03-28 18:16:33 +0200

    am completat interfata crearii unei postari

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 export default function App() {
   return (
@@ -23,7 +25,9 @@ export default function App() {
         <Route path = "/dashboard" element = {<Dashboard/>}/>
       </Route>
       <Route path = "/sign-in" element = {<SignIn />}/>
-      
+      <Route element = {<OnlyAdminPrivateRoute/>}>
+        <Route path = "/create-post" element = {<CreatePost/>}/>
+      </Route>
       
     </Routes>
     <Footer/>

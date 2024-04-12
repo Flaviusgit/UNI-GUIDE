a09446cec62ebc3304ed20413643cd9e86c18760
Flavius <flavius.carpean@student.upt.ro>
2024-04-12 19:17:07 +0300

    modificari mici

--- api/controllers/question.controller.js
+++ api/controllers/question.controller.js
@@ -1,23 +1,23 @@
 import { errorHandler } from '../utils/error.js';
 import Question from '../models/question.model.js';
 
-export const getQuestion = async (req, res) => {
+export const getQuestion = async (req, res, next) => {
     try {
         const count = await Question.countDocuments();
         const randomIndex = Math.floor(Math.random() * count);
         const question = await Question.findOne().skip(randomIndex);
-        res.status(200).res.json(question);
+        res.status(200).json(question);
     } catch (error) {
-        next(error)
+        next(error); 
     }
 };
 
-export const addQuestion = async (req, res) => {
+export const addQuestion = async (req, res, next) => {
     try {
         const { text, options, correctOption } = req.body;
         const newQuestion = await Question.create({ text, options, correctOption });
         res.status(200).json(newQuestion);
     } catch (error) {
-        next(error)
+        next(error); 
     }
 };
--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,61 +1,117 @@
 import React, { useState, useEffect } from 'react';
 
-const Quiz = () => {
-  const [question, setQuestion] = useState(null);
-  const [selectedOption, setSelectedOption] = useState(null);
+export default function Quiz() {
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(1);
+  const [answeredQuestions, setAnsweredQuestions] = useState([]);
+  const [displayedQuestionIndices, setDisplayedQuestionIndices] = useState([]);
 
   useEffect(() => {
-    fetchQuestion();
-  }, []);
-
-  const fetchQuestion = async () => {
-    try {
-      // Aici vei face o cerere către server pentru a obține o întrebare reală din baza de date
-      // În acest exemplu, vom simula datele
-      const sampleQuestion = {
-        text: 'Care este capitala României?',
-        options: ['București', 'Budapesta', 'Sofia', 'Belgrad'],
-      };
-      setQuestion(sampleQuestion);
-    } catch (error) {
-      console.error('Error fetching question:', error);
+    async function fetchRandomQuestion() {
+      try {
+        const response = await fetch('/api/quiz/getQuestion');
+        if (!response.ok) {
+          throw new Error('Failed to fetch random question');
+        }
+        const data = await response.json();
+        setCurrentQuestion(data);
+        setStartQuiz(true);
+      } catch (error) {
+        console.error('Error fetching random question:', error);
+      }
     }
+
+    if (startQuiz && !currentQuestion) {
+      fetchRandomQuestion();
+    }
+  }, [startQuiz, currentQuestion]);
+
+  const handleNextQuestion = () => {
+    setAnsweredQuestions([...answeredQuestions, currentQuestion]);
+    setCurrentQuestion(null); 
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
 
-  const handleAnswer = (option) => {
-    setSelectedOption(option);
+  const handlePreviousQuestion = () => {
+    setCurrentQuestion(answeredQuestions[currentQuestionIndex - 2]); 
+    setCurrentQuestionIndex(currentQuestionIndex - 1);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Trimite răspunsul către server sau gestionează-l
-  };
+  useEffect(() => {
+    async function fetchRandomQuestion() {
+      try {
+        const response = await fetch('/api/quiz/getQuestion');
+        if (!response.ok) {
+          throw new Error('Failed to fetch random question');
+        }
+        const data = await response.json();
+        if (answeredQuestions.some(ansQuestion => ansQuestion._id === data._id)) {
+          console.log('Intrebarea a mai fost afisata anterior. Se cauta alta intrebare...');
+          fetchRandomQuestion(); 
+        } else {
+          setCurrentQuestion(data);
+          setStartQuiz(true);
+        }
+      } catch (error) {
+        console.error('Error fetching random question:', error);
+      }
+    }
+  
+    if (startQuiz && !currentQuestion) {
+      fetchRandomQuestion();
+    }
+  }, [startQuiz, currentQuestion, answeredQuestions]);
+  
+  
 
+ 
   return (
-    <div>
-      {question && (
-        <form onSubmit={handleSubmit}>
-          <h2>{question.text}</h2>
-          <ul>
-            {question.options.map((option, index) => (
-              <li key={index}>
-                <label>
-                  <input
-                    type="radio"
-                    value={option}
-                    checked={selectedOption === option}
-                    onChange={() => handleAnswer(option)}
-                  />
-                  {option}
-                </label>
-              </li>
-            ))}
-          </ul>
-          <button type="submit">Submit</button>
-        </form>
-      )}
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="max-w-md w-full p-8 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-center mb-4">Quiz</h1>
+        {!startQuiz ? (
+          <div>
+            <p className="text-lg mb-4 whitespace-nowrap">Pentru a începe testul, apăsați butonul de mai jos.</p>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+              onClick={() => setStartQuiz(true)}
+            >
+              Start Quiz
+            </button>
+          </div>
+        ) : (
+          <div> 
+            <p className="text-lg">{currentQuestionIndex}.{currentQuestion?.text}</p>
+            <ul>
+              {currentQuestion?.options.map((option, index) => (
+                <li key={index}>
+                  <label className="inline-flex items-center mt-3">
+                    <input type="radio" className="form-radio" name="option" value={option} />
+                    <span className="ml-2">{option}</span>
+                  </label>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-between mt-4">
+              {currentQuestionIndex !== 1 && (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2 mr-2"
+                  onClick={handlePreviousQuestion}
+                >
+                  Back
+                </button>
+              )}
+              <button
+                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-${currentQuestionIndex === 1 ? 'full' : '1/2'} ml-${currentQuestionIndex === 1 ? '0' : '2'}`}
+                onClick={handleNextQuestion}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-};
-
-export default Quiz;
+}

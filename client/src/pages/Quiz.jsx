d2d7d40d4d461708f4559aa369a7c2f2f5a94e95
Flavius <flavius.carpean@student.upt.ro>
2024-04-16 00:06:06 +0300

    Am adaugat scorul pentru fiecare raspuns corect

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -5,9 +5,12 @@ export default function Quiz() {
   const [username, setUsername] = useState('');
   const [startQuiz, setStartQuiz] = useState(false);
   const [questions, setQuestions] = useState([]); 
+  const [userAnswers, setUserAnswers] = useState([]); 
   const [currentQuestion, setCurrentQuestion] = useState(null);
   const [selectedOption, setSelectedOption] = useState('');
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [finalScore, setFinalScore] = useState(0);
+  const [showResults, setShowResults] = useState(false);
 
   const fetchQuestions = async () => {
     try {
@@ -35,30 +38,78 @@ export default function Quiz() {
     if (nextIndex < questions.length) {
       setQuestionIndex(nextIndex);
       setCurrentQuestion(questions[nextIndex]);
+      setSelectedOption(userAnswers[nextIndex] || '');
     } else {
-      alert('Ai ajuns la finalul quizului!');
+      
+      setStartQuiz(false);
     }
   };
+  
 
   const handlePreviousQuestion = () => {
     const previousIndex = questionIndex - 1;
     if (previousIndex >= 0) {
       setQuestionIndex(previousIndex);
       setCurrentQuestion(questions[previousIndex]);
+      
+      setSelectedOption(userAnswers[previousIndex] || '');
     }
   };
   
+
   const handleRestartQuiz = () => {
     setStartQuiz(false);
     setCurrentQuestion(null);
     setSelectedOption('');
     setQuestionIndex(0);
+    setUserAnswers([]);
+    setShowResults(false); 
   };
 
   const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
+    const value = event.target.value;
+    setSelectedOption(value);
+  
 
+    const updatedAnswers = [...userAnswers];
+    updatedAnswers[questionIndex] = value;
+    setUserAnswers(updatedAnswers);
+  };
+  
+  const handleSubmitQuiz = () => {
+   
+    const updatedUserAnswers = [...userAnswers];
+    updatedUserAnswers[questionIndex] = selectedOption;
+  
+    
+    if (updatedUserAnswers.length < questions.length || updatedUserAnswers.includes(undefined) || updatedUserAnswers.includes('')) {
+      alert('Te rugăm să răspunzi la toate întrebările înainte de a finaliza quiz-ul.');
+      return; 
+    }
+  
+ 
+    const score = calculateScore(updatedUserAnswers);
+    setFinalScore(score);
+  
+   
+    setUserAnswers(updatedUserAnswers);
+    setStartQuiz(false);
+    setShowResults(true);
+  };
+  
+  
+  
+  
+
+  const calculateScore = (answers) => {
+    let score = 0;
+    for (let i = 0; i < answers.length; i++) {
+      if (answers[i] === questions[i].correctOption) {
+        score++;
+      }
+    }
+    return score;
+  };
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="relative w-full max-w-6xl p-8 bg-gray-800 text-white rounded-lg shadow-lg">
@@ -105,45 +156,73 @@ export default function Quiz() {
                 )}
               </div>
               <div className="absolute bottom-0 right-0 mb-4 mr-4">
-                <Button
-                  gradientDuoTone="greenToBlue"
-                  onClick={handleNextQuestion}
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
-                >
-                  Înainte
-                </Button>
+                {questionIndex === questions.length - 1 ? (
+                  <Button
+                    gradientDuoTone="greenToBlue"
+                    onClick={handleSubmitQuiz}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+                  >
+                    Submit
+                  </Button>
+                ) : (
+                  <Button
+                    gradientDuoTone="greenToBlue"
+                    onClick={handleNextQuestion}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+                  >
+                    Înainte
+                  </Button>
+                )}
               </div>
             </div>
           ) : (
-            <form onSubmit={handleStartQuiz} className={startQuiz ? "hidden" : "space-y-4"}>
-              <h1 className="text-3xl text-center font-semibold border-b-2 border-green-500 pb-2">
-                Quiz Application
-              </h1>
-              <ul className="list-disc text-sm space-y-2 mt-4 ml-5">
-                <li>Testul conține 12 întrebări.</li>
-                <li>Fiecare întrebare are 4 opțiuni de răspuns, unul singur fiind corect.</li>
-                <li>Poți să navighezi prin întrebări înainte să finalizezi testul.</li>
-                <li>Rezultatele o să fie afișate la final.</li>
-              </ul>
-              <div className="flex items-center mt-4">
-                <input 
-                  type="text" 
-                  id="username" 
-                  name="username"
-                  placeholder="Username" 
-                  className="w-1/2 p-2 text-black rounded-lg" 
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required 
-                />
-                <button 
-                  type="submit"
-                  className="w-1/2 ml-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
-                >
-                  Start Quiz
-                </button>
-              </div>
-            </form>
+            <>
+              {showResults ? (
+                <div className="text-center">
+                  <h1 className="text-3xl font-semibold mb-4">Rezultate</h1>
+                  <p className="text-lg mb-2">
+                    Scorul tău este: {finalScore}/{questions.length}
+                  </p>
+                  <Button
+                    gradientDuoTone="greenToBlue"
+                    onClick={handleRestartQuiz}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+                  >
+                    Reia quiz-ul
+                  </Button>
+                </div>
+              ) : (
+                <form onSubmit={handleStartQuiz} className={startQuiz ? "hidden" : "space-y-4"}>
+                  <h1 className="text-3xl text-center font-semibold border-b-2 border-green-500 pb-2">
+                    Quiz Application
+                  </h1>
+                  <ul className="list-disc text-sm space-y-2 mt-4 ml-5">
+                    <li>Testul conține 12 întrebări.</li>
+                    <li>Fiecare întrebare are 4 opțiuni de răspuns, unul singur fiind corect.</li>
+                    <li>Poți să navighezi prin întrebări înainte să finalizezi testul.</li>
+                    <li>Rezultatele o să fie afișate la final.</li>
+                  </ul>
+                  <div className="flex items-center mt-4">
+                    <input 
+                      type="text" 
+                      id="username" 
+                      name="username"
+                      placeholder="Username" 
+                      className="w-1/2 p-2 text-black rounded-lg" 
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required 
+                    />
+                    <button 
+                      type="submit"
+                      className="w-1/2 ml-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+                    >
+                      Start Quiz
+                    </button>
+                  </div>
+                </form>
+              )}
+            </>
           )}
         </div>
       </div>
